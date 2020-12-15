8870b53ef7e7507557c22be1d72bc81b3c035072
jamcg <7832952+jamcg@users.noreply.github.com>
2020-12-15 12:53:34 +0000

    Update app.js
    live demo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ http.createServer(function (request, response) {
    // Send the 200 : OK
    response.writeHead(200, {'Content-Type': 'text/plain'});
    // Send the response body
-   response.end('Hello World!\n');
+   response.end('Hello World\n');
 }).listen(8080);
 
 // Console will print the message
